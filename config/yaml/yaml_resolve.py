a094651e827ec1bfd9a1bb569bd400338024837e
hj <864655329@qq.com>
2023-02-02 09:17:06 +0800

    添加每日持仓 名称转换

--- config/yaml/yaml_resolve.py
+++ config/yaml/yaml_resolve.py
@@ -3,7 +3,7 @@ import yaml
 
 class YamlResolve:
     def __init__(self):
-        with open('application.yaml') as f:
+        with open('application.yaml', encoding="utf-8") as f:
             self.data = yaml.load(f, Loader=yaml.FullLoader)
 
     def get_local_config(self):
